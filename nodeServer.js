5101ef22a1526e605bc018dedfa1e1487e40846d
alexromandan <alexthe_roman@yahoo.com>
2017-08-25 22:36:31 +0300

    Alex re-deploy

--- nodeServer.js
+++ nodeServer.js
@@ -12,21 +12,8 @@ const app = express(),
 	server = module.exports = http.createServer( app );
 require('./config/express')(express, app, server, settings, passport);
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-	socket.on('chat message', function(msg){
-	io.emit('chat message: ' + msg);
-	});
-  });
-
 //Socket.io
 require('./lib/sockets')(socket, server);
+
 
 console.log('Running in ' + (process.env.NODE_ENV || 'development') + ' mode @ ' + settings.uri);
