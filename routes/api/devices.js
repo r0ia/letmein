fbd4bcfef6c82a83943f0c5b78644bb3df2a2eb1
MikeD <mikeddolha@gmail.com>
2017-08-27 17:16:28 +0300

    Fix socket management

--- routes/api/devices.js
+++ routes/api/devices.js
@@ -1,4 +1,4 @@
-module.exports = function (io) {
+module.exports = function(io) {
 
     var express = require('express');
     var router = express.Router();
@@ -6,23 +6,35 @@ module.exports = function (io) {
     var Device = require('../../models/device'),
         DeviceModel = Device.model('Devices');
 
+    var socket = null;
+
+    io.on('connection', function(mySoc) {
+        socket = mySoc;
+    });
+
     router.route('/devices')
         // create device
-        .post(function (req, res) {
+        .post(function(req, res) {
             try {
-                DeviceModel.find({ deviceId: req.body.deviceId }, function (err, grantedDevice) {
+                DeviceModel.find({
+                    deviceId: req.body.deviceId
+                }, function(err, grantedDevice) {
                     if (err)
                         res.send(err);
                     if (grantedDevice.length > 0) {
-                        res.json({ message: 'Device already created!' });
+                        res.json({
+                            message: 'Device already created!'
+                        });
                     } else {
                         var new_device = new DeviceModel(req.body);
-                        new_device.save(function (err) {
+                        new_device.save(function(err) {
                             console.log('am intrat');
                             if (err)
                                 res.send(err);
 
-                            res.json({ message: 'Device created!' });
+                            res.json({
+                                message: 'Device created!'
+                            });
                         });
                     }
                 });
@@ -31,71 +43,81 @@ module.exports = function (io) {
             }
         })
         // find devices
-        .get(function (req, res) {
+        .get(function(req, res) {
             let token = req.headers['authorization'];
-            if(token) {
-                if(token.match(config.params.token)) {
-                    DeviceModel.find(function (err, devices) {
+            if (token) {
+                if (token.match(config.params.token)) {
+                    DeviceModel.find(function(err, devices) {
                         if (err)
                             res.send(err);
                         res.json(devices);
                     });
                 } else {
                     res.status = 401;
-                    res.json({message: "Unauthorized"});
+                    res.json({
+                        message: "Unauthorized"
+                    });
                 }
             } else {
                 res.status = 401;
-                res.json({message: "Unauthorized"});
+                res.json({
+                    message: "Unauthorized"
+                });
             }
         });
 
     router.route('/devices/:device_id')
         // get the device by id
-        .get(function (req, res) {
-            DeviceModel.find({ deviceId: req.params.device_id }, function (err, grantedDevice) {
+        .get(function(req, res) {
+            DeviceModel.find({
+                deviceId: req.params.device_id
+            }, function(err, grantedDevice) {
                 if (err)
                     res.send(err);
-                if(grantedDevice.length > 0) {
+                if (grantedDevice.length > 0) {
                     var userDetails = grantedDevice.pop().user;
-                    io.sockets.emit('unlock', userDetails);
-                    io.sockets.on('unlocked', function(unlocked){
-                        console.log(unlocked);
-                        if(unlocked)
+
+                    socket.emit('unlock', userDetails);
+
+                    socket.on('unlocked', function(unlocked) {
+                        if (unlocked) {
                             res.json(userDetails);
+                        }
                     });
                 }
             });
         })
-        // update my device        
-        .put(function (req, res) {
-            DeviceModel.updateOne(
-                {
+        // update my device
+        .put(function(req, res) {
+            DeviceModel.updateOne({
                     deviceId: req.params.device_id
-                },
-                {
+                }, {
                     $set: {
                         'user.firstName': req.body.firstName,
                         'user.lastName': req.body.lastName
                     },
                 },
-                function (err) {
+                function(err) {
                     if (err)
                         res.send(err);
-                    res.json({ message: 'Device updated!' });
+                    res.json({
+                        message: 'Device updated!'
+                    });
                 });
         })
-        .delete(function (req, res) {
+        .delete(function(req, res) {
             DeviceModel.remove({
                 deviceId: req.params.device_id
-            }, function (err, bear) {
+            }, function(err, bear) {
                 if (err)
                     res.send(err);
-                res.json({ message: 'Successfully deleted device!' });
+                res.json({
+                    message: 'Successfully deleted device!'
+                });
             });
         });
 
-    router.use(function (req, res, next) {
+    router.use(function(req, res, next) {
         // do logging
         console.log('Something is happening.');
         next(); // make sure we go to the next routes and don't stop here
